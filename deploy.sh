daea2927542aa121d2ea3f545d59c0af32478697
Gannon <gannon.barnett+mac@gmail.com>
2023-07-16 20:29:08 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ echo "daysoffplanner.com" > build/CNAME
 git add .
 git commit -m "deploy"
 git push
-git subtree split --prefix build -b gh-pages 
+git subtree split --prefix source -b gh-pages 
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages 
